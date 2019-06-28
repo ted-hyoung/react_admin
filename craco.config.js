629608348b457b8c1dfcc476fdcf3cf120b6e224
Byungha <lbh770@imform.co.kr>
2019-06-28 17:48:13 +0900

    공통 모듈 수정

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,16 @@
 module.exports = {
+  typescript: {
+    enableTypeChecking: true,
+  },
   plugins: [
     {
-      plugin: require("craco-antd"),
+      plugin: require('craco-antd'),
       options: {
         customizeTheme: {
-          "@primary-color": "#1DA57A",
-          "@link-color": "#1DA57A"
-        }
-      }
-    }
-  ]
+          '@primary-color': '#1DA57A',
+          '@link-color': '#1DA57A',
+        },
+      },
+    },
+  ],
 };
