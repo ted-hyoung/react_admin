fd0332d0bd08fc6fd0d5e9f8ffea87e196dac3ba
kth216 <kth216@imform.co.kr>
2019-10-13 23:40:46 +0900

    FRC-420 제품매출 검색조건 개선

--- src/store/saga/productSaga.ts
+++ src/store/saga/productSaga.ts
@@ -100,7 +100,9 @@ function* getProductStatistics(action: RequestAsyncAction) {
     const res = yield call(() =>
       Api.get('/management/products/statistics', {
         params: {
-          ...searchCondition,
+          events:[
+            ...searchCondition,
+          ]
         },
         paramsSerializer: (params: any) => qs.stringify(params, { arrayFormat: 'indices', allowDots: true }),
       }),
@@ -117,7 +119,9 @@ function* getProductStatisticsExcel(action: RequestAsyncAction) {
     const res = yield call(() =>
       Api.get('/management/products/statistics', {
         params: {
-          ...searchCondition,
+          events:[
+            ...searchCondition,
+          ]
         },
         paramsSerializer: (params: any) => qs.stringify(params, { arrayFormat: 'indices', allowDots: true }),
       }),
