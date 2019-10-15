1d456eda4581fde221aee7a2893a8f921b1a05c2
mi-reu <mireu719@imform.co.kr>
2019-10-15 09:06:51 +0900

    Enum Value 타입 변경

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,5 @@
+import { Indexable } from 'models';
+
 export * from './EventStatus';
 export * from './QnaStatus';
 export * from './QnaOrderType';
@@ -12,3 +14,9 @@ export * from './DateRange';
 export * from './CardCode';
 export * from './Role';
 export * from './ImportanceCode';
+
+export function getEnumKeyByValue<T extends Indexable, V>(enumType: T, enumValue: V): string | undefined {
+  const keys = Object.keys(enumType).filter(k => enumType[k] === enumValue);
+
+  return keys.length > 0 ? keys[0] : undefined;
+}
