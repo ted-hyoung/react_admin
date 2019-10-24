fa1dd3548f51d47c2f0f326019c764ca17e9e6a7
mi-reu <mireu719@imform.co.kr>
2019-10-24 15:26:34 +0900

    FRC-415  텍스트 크기 수정

---
+++ src/components/TextEditor/CustomToolbar.tsx
@@ -0,0 +1,90 @@
+// base
+import React, { useState, useRef } from 'react';
+
+// modules
+import { Icon } from 'antd';
+
+// components
+import { InstagramIcon } from 'components/Icons';
+import InstagramForm from './InstagramForm';
+
+interface CustomToolbarProps {
+  name: string;
+  instagramTool: boolean;
+  saveSelection: () => void;
+  imageHandler: (file: File) => void;
+  instagramHandler: (url: string) => void;
+}
+
+const CustomToolbar = (props: CustomToolbarProps) => {
+  const { name, instagramTool, saveSelection, imageHandler, instagramHandler } = props;
+
+  const [visible, setVisible] = useState(false);
+
+  const fileInputEl = useRef<HTMLInputElement>(null);
+
+  const handleClickFileInput = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (fileInputEl.current) {
+      fileInputEl.current.click();
+      saveSelection();
+    }
+  };
+
+  return (
+    <div id={`${name}-toolbar`}>
+      <span className="ql-formats">
+        <select className="ql-size" />
+      </span>
+      <span className="ql-formats">
+        {/* 기획팀 요청으로 인한 주석 처리 */}
+        {/* <select className="ql-header" /> */}
+        <button type="button" className="ql-bold" />
+        <button type="button" className="ql-italic" />
+        <button type="button" className="ql-underline" />
+      </span>
+      <span className="ql-formats">
+        <select className="ql-align" />
+      </span>
+      <span className="ql-formats">
+        <select className="ql-color" />
+        <select className="ql-background" />
+      </span>
+      <span className="ql-formats">
+        <button type="button" onClick={handleClickFileInput}>
+          <InstagramIcon />
+          <input
+            ref={fileInputEl}
+            style={{ display: 'none' }}
+            type="file"
+            onClick={e => {
+              e.stopPropagation();
+              e.currentTarget.value = '';
+            }}
+            onChange={e => {
+              if (!e.target.files) {
+                return false;
+              }
+              imageHandler(e.target.files[0]);
+            }}
+          />
+        </button>
+        <button type="button" className="ql-video" />
+        <button type="button" className="ql-link" />
+        {instagramTool && (
+          <button
+            type="button"
+            onClick={() => {
+              setVisible(true);
+              saveSelection();
+            }}
+          >
+            <Icon type="instagram" />
+          </button>
+        )}
+      </span>
+      {instagramTool && <InstagramForm visible={visible} onOk={instagramHandler} onCancel={() => setVisible(false)} />}
+    </div>
+  );
+};
+
+export default CustomToolbar;
